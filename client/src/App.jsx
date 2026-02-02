b3b2131830a9d75cc34be0d39cc99a6107f47583
khamsing5161 <thasing436@gmail.com>
2026-02-02 09:03:18 +0700

    me

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Menu from './pages/Menu'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Menu />
+
+    </>
+  )
+}
+
+export default App
